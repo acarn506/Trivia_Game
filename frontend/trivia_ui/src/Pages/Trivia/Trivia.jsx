938d8676bbabd6b96b969f536e2f8fd06191902d
Tim Myers <tdmsportplayer@yahoo.com>
2020-07-11 21:04:29 -0700

    question styling

--- frontend/trivia_ui/src/Pages/Trivia/Trivia.jsx
+++ frontend/trivia_ui/src/Pages/Trivia/Trivia.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Trivia.css";
 
 const Trivia = props => {
   return (
     <div>
       <h1 className="genre">Genre: Sports</h1>
+      <div className="q-and-a">
+        <h3 className="question">Who is the best football team of all time?</h3>
+        <div className="all-answers">
+          <button className="answer">New England Patriots</button>
+          <button className="answer">Pittsburgh Steelers</button>
+          <button className="answer">Dallas Cowboys</button>
+          <button className="answer">Oakland Raiders</button>
+        </div>
+      </div>
     </div>
   );
 };
