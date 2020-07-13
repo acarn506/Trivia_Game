f027cf2c4d71c80b40f04d9a1eedc4cf00c3474b
Tim Myers <tdmsportplayer@yahoo.com>
2020-07-12 22:55:05 -0700

    Question Functionality

--- frontend/trivia_ui/src/Pages/Trivia/Trivia.jsx
+++ frontend/trivia_ui/src/Pages/Trivia/Trivia.jsx
@@ -1,21 +1,94 @@
-import React from "react";
+import React, { Component }  from "react";
 import "./Trivia.css";
 
-const Trivia = props => {
+const wrongColor = {
+  background : "red"
+}
+const rightColor = {
+  background : "green"
+}
+
+const normalColor = {
+  background : "yellow"
+}
+
+class Trivia extends Component {
+    state = {
+      buttons : [ 
+        {
+          name : 'New England Patriots',
+          buttonColor : normalColor,
+          key : 0
+        },
+        {
+          name : 'Pittsburgh Steelers',
+          buttonColor : normalColor,
+          key : 1
+        },
+        {
+          name : 'Dallas Cowboys',
+          buttonColor : normalColor,
+          key : 2
+        },
+        {
+          name : 'Oakland Raiders',
+          buttonColor : normalColor,
+          key : 3
+        }
+      ]
+    }
+
+    correctChoice = (index) => {
+      let buttonIndex = this.state.buttons.findIndex((button)=>{
+        return index === button.key
+      })
+      let copyButtons = [...this.state.buttons] 
+      copyButtons[buttonIndex].buttonColor = rightColor
+      this.setState({
+        buttons:copyButtons
+      })
+    }
+    
+    incorrectChoice = (index) => {
+      let buttonIndex = this.state.buttons.findIndex((button)=>{
+        return index === button.key
+      })
+      let copyButtons = [...this.state.buttons] 
+      copyButtons[buttonIndex].buttonColor = wrongColor
+      this.setState({
+        buttons:copyButtons
+      })
+    }
+    
+    render() {
+
+  let rightAnswer = 'Oakland Raiders'
+
+let finishedButtons = this.state.buttons.map((button, index)=>{
+  return ( 
+    <React.Fragment>
+      { button.name === rightAnswer 
+        ?
+        <button key= {button.key} style={button.buttonColor} onClick= {() =>  this.correctChoice(index)} className="choice">{button.name}</button>
+        :         
+        <button key= {button.key} style={button.buttonColor} onClick= {() =>  this.incorrectChoice(index)} className="choice">{button.name}</button>
+      }
+    </React.Fragment>
+  )
+})   
+
   return (
     <div>
       <h1 className="genre">Genre: Sports</h1>
       <div className="q-and-a">
         <h3 className="question">Who is the best football team of all time?</h3>
-        <div className="all-answers">
-          <button className="answer">New England Patriots</button>
-          <button className="answer">Pittsburgh Steelers</button>
-          <button className="answer">Dallas Cowboys</button>
-          <button className="answer">Oakland Raiders</button>
+          <div className="all-choices">
+          {finishedButtons}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
+}
 
 export default Trivia;
